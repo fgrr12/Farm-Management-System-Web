83cc29581abcbd44075e8ecd22d858f188706dd2
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2024-03-30 23:17:40 -0600

    fix: remove validations

--- src/pages/Animal/Animal.page.tsx
+++ src/pages/Animal/Animal.page.tsx
@@ -142,79 +142,73 @@ export const Animal: FC = () => {
 					</S.ImageContainer>
 				</S.AnimalContainer>
 
-				{animal.healthRecords.length > 0 && (
-					<S.TableContainer>
-						<S.CenterTitle>
-							<S.Label>Health Records</S.Label>
-							{user && (
-								<ActionButton
-									title="Add Health Record"
-									icon="i-material-symbols-add-circle-outline"
-								/>
-							)}
-						</S.CenterTitle>
-						<Table>
-							<Table.Head>
-								<Table.Row>
-									<Table.HeadCell>Reason</Table.HeadCell>
-									<Table.HeadCell>Notes</Table.HeadCell>
-									<Table.HeadCell>Type</Table.HeadCell>
-									<Table.HeadCell>Reviewed By</Table.HeadCell>
-									<Table.HeadCell>Date</Table.HeadCell>
-									<Table.HeadCell>Weight</Table.HeadCell>
-									<Table.HeadCell>Temperature</Table.HeadCell>
-									<Table.HeadCell>Medication</Table.HeadCell>
-									<Table.HeadCell>Dosage</Table.HeadCell>
-									<Table.HeadCell>Frequency</Table.HeadCell>
-									<Table.HeadCell>Duration</Table.HeadCell>
-									{user && <Table.HeadCell>Actions</Table.HeadCell>}
-								</Table.Row>
-							</Table.Head>
-							<Table.Body>
-								{animal.healthRecords.map((healthRecord) => (
-									<Table.Row key={crypto.randomUUID()}>
-										<Table.Cell data-title="Reason">{healthRecord.reason}</Table.Cell>
-										<Table.Cell data-title="Notes">{healthRecord.notes}</Table.Cell>
-										<Table.Cell data-title="Type">{healthRecord.type}</Table.Cell>
-										<Table.Cell data-title="Reviewed By">{healthRecord.reviewedBy}</Table.Cell>
-										<Table.Cell data-title="Date">
-											{dayjs(healthRecord.date).format('MM/DD/YYYY')}
+				<S.TableContainer>
+					<S.CenterTitle>
+						<S.Label>Health Records</S.Label>
+						{user && (
+							<ActionButton
+								title="Add Health Record"
+								icon="i-material-symbols-add-circle-outline"
+							/>
+						)}
+					</S.CenterTitle>
+					<Table>
+						<Table.Head>
+							<Table.Row>
+								<Table.HeadCell>Reason</Table.HeadCell>
+								<Table.HeadCell>Notes</Table.HeadCell>
+								<Table.HeadCell>Type</Table.HeadCell>
+								<Table.HeadCell>Reviewed By</Table.HeadCell>
+								<Table.HeadCell>Date</Table.HeadCell>
+								<Table.HeadCell>Weight</Table.HeadCell>
+								<Table.HeadCell>Temperature</Table.HeadCell>
+								<Table.HeadCell>Medication</Table.HeadCell>
+								<Table.HeadCell>Dosage</Table.HeadCell>
+								<Table.HeadCell>Frequency</Table.HeadCell>
+								<Table.HeadCell>Duration</Table.HeadCell>
+								{user && <Table.HeadCell>Actions</Table.HeadCell>}
+							</Table.Row>
+						</Table.Head>
+						<Table.Body>
+							{animal.healthRecords.map((healthRecord) => (
+								<Table.Row key={crypto.randomUUID()}>
+									<Table.Cell data-title="Reason">{healthRecord.reason}</Table.Cell>
+									<Table.Cell data-title="Notes">{healthRecord.notes}</Table.Cell>
+									<Table.Cell data-title="Type">{healthRecord.type}</Table.Cell>
+									<Table.Cell data-title="Reviewed By">{healthRecord.reviewedBy}</Table.Cell>
+									<Table.Cell data-title="Date">
+										{dayjs(healthRecord.date).format('MM/DD/YYYY')}
+									</Table.Cell>
+									<Table.Cell data-title="Weight">{healthRecord.weight}</Table.Cell>
+									<Table.Cell data-title="Temperature">{healthRecord.temperature}</Table.Cell>
+									<Table.Cell data-title="Medication">{healthRecord.medication}</Table.Cell>
+									<Table.Cell data-title="Dosage">{healthRecord.dosage}</Table.Cell>
+									<Table.Cell data-title="Frequency">{healthRecord.frequency}</Table.Cell>
+									<Table.Cell data-title="Duration">{healthRecord.duration}</Table.Cell>
+									{user && (
+										<Table.Cell data-title="Actions">
+											<ActionButton title="Edit" icon="i-material-symbols-edit-square-outline" />
+											<ActionButton title="Delete" icon="i-material-symbols-delete-outline" />
 										</Table.Cell>
-										<Table.Cell data-title="Weight">{healthRecord.weight}</Table.Cell>
-										<Table.Cell data-title="Temperature">{healthRecord.temperature}</Table.Cell>
-										<Table.Cell data-title="Medication">{healthRecord.medication}</Table.Cell>
-										<Table.Cell data-title="Dosage">{healthRecord.dosage}</Table.Cell>
-										<Table.Cell data-title="Frequency">{healthRecord.frequency}</Table.Cell>
-										<Table.Cell data-title="Duration">{healthRecord.duration}</Table.Cell>
-										{user && (
-											<Table.Cell data-title="Actions">
-												<ActionButton title="Edit" icon="i-material-symbols-edit-square-outline" />
-												<ActionButton title="Delete" icon="i-material-symbols-delete-outline" />
-											</Table.Cell>
-										)}
-									</Table.Row>
-								))}
-							</Table.Body>
-						</Table>
-					</S.TableContainer>
-				)}
+									)}
+								</Table.Row>
+							))}
+						</Table.Body>
+					</Table>
+				</S.TableContainer>
 
 				<S.InfoTableContainer>
-					{animal.relatedAnimals.parents.length !== 0 && (
-						<RelatedAnimalsTable
-							title="Parents Related Animals"
-							animals={animal.relatedAnimals.parents!}
-							user={user}
-						/>
-					)}
+					<RelatedAnimalsTable
+						title="Parents Related Animals"
+						animals={animal.relatedAnimals.parents!}
+						user={user}
+					/>
 
-					{animal.relatedAnimals.children.length !== 0 && (
-						<RelatedAnimalsTable
-							title="Children Related Animals"
-							animals={animal.relatedAnimals.children!}
-							user={user}
-						/>
-					)}
+					<RelatedAnimalsTable
+						title="Children Related Animals"
+						animals={animal.relatedAnimals.children!}
+						user={user}
+					/>
 				</S.InfoTableContainer>
 			</S.Container>
 		</>
