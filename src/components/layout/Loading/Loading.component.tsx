5441d0cf183cbb23c0c04290352a1384db1724d3
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2025-04-17 12:32:05 -0600

    fix: change loading design

--- src/components/layout/Loading/Loading.component.tsx
+++ src/components/layout/Loading/Loading.component.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
-import * as S from './Loading.styles'
 import type { LoadingProps, LoadingRef } from './Loading.types'
 
 export const Loading: FC<LoadingProps> = ({ open, ...rest }) => {
@@ -8,20 +7,37 @@ export const Loading: FC<LoadingProps> = ({ open, ...rest }) => {
 	const { loadingRef } = useLoading(open)
 
 	return (
-		<S.Loading ref={loadingRef} {...rest}>
-			<S.TextContainer>
-				<S.Letter>{t('loading')}</S.Letter>
-				<S.Dot>.</S.Dot>
-				<S.Dot>.</S.Dot>
-				<S.Dot>.</S.Dot>
-			</S.TextContainer>
-			<S.XSpinner>
-				<S.Icon className="i-fluent-emoji-flat-cow" />
-				<S.Icon className="i-emojione-chicken" />
-				<S.Icon className="i-fxemoji-sheep" />
-				<S.Icon className="i-emojione-goat" />
-			</S.XSpinner>
-		</S.Loading>
+		<dialog
+			ref={loadingRef}
+			{...rest}
+			className="
+				fixed
+				top-1/2
+				left-1/2
+				-translate-x-1/2
+				-translate-y-1/2
+				border-none
+				bg-transparent
+				outline-none
+				overflow-hidden
+				backdrop-blur-xl
+				backdrop:bg-neutral-500
+				"
+		>
+			<div className="flex justify-center items-center h-10 mb-8!">
+				<span className="text-white text-4xl mx-1">{t('loading')}</span>
+				<span className="text-white text-5xl mx-1 animate-dot-pulse delay-0">.</span>
+				<span className="text-white text-5xl mx-1 animate-dot-pulse delay-100">.</span>
+				<span className="text-white text-5xl mx-1 animate-dot-pulse delay-200">.</span>
+			</div>
+
+			<div className="flex justify-center items-center w-screen">
+				<i className="i-fluent-emoji-flat-cow w-12! h-12! mx-2 animate-slide-left delay-0" />
+				<i className="i-emojione-chicken w-12! h-12! mx-2 animate-slide-left delay-100" />
+				<i className="i-fxemoji-sheep w-12! h-12! mx-2 animate-slide-left delay-200" />
+				<i className="i-emojione-goat w-12! h-12! mx-2 animate-slide-left delay-300" />
+			</div>
+		</dialog>
 	)
 }
 
