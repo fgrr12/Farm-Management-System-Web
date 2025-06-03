ca168f88220e5367c89f89e790859b0e338a4801
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2025-06-03 17:48:15 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'alert-info',
+    'alert-success',
+    'alert-error',
+    'alert-warning',
+  ],
+  plugins: [require("daisyui")],
+}
