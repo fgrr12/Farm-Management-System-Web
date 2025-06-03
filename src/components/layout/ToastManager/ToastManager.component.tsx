7694d2969dfcbb0e7fcb43aad1650fd1a8157b2a
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2025-06-03 13:59:20 -0600

    feat: add gsap and toast

---
+++ src/components/layout/ToastManager/ToastManager.component.tsx
@@ -0,0 +1,41 @@
+import { Toast } from '@/components/ui/Toast'
+import { useCallback, useState } from 'react'
+import { ToastItem } from './ToastManager.types'
+
+export const ToastManager = () => {
+	const [toasts, setToasts] = useState<ToastItem[]>([])
+
+	const addToast = (toast: Omit<ToastItem, 'id'>) => {
+		setToasts((prev) => [...prev, { ...toast, id: Math.random().toString() }])
+	}
+
+	const removeToast = useCallback((id: string) => {
+		setToasts((prev) => prev.filter((t) => t.id !== id))
+	}, [])
+
+	return (
+		<>
+			<div className="fixed top-4 right-4 space-y-2 z-50 w-full max-w-sm">
+				{toasts.map((toast) => (
+					<Toast key={toast.id} {...toast} onClose={removeToast} />
+				))}
+			</div>
+
+			{/* Botón de prueba (opcional) */}
+			<div className="fixed bottom-4 right-4">
+				<button
+					className="btn btn-primary"
+					onClick={() =>
+						addToast({
+							message: 'Nueva notificación',
+							type: 'success',
+							duration: 3000,
+						})
+					}
+				>
+					Agregar Toast
+				</button>
+			</div>
+		</>
+	)
+}
