d1e39d61d5f54b757985716b05c815a03afd2383
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2025-06-03 22:20:31 -0600

    feat: add animations

--- src/components/ui/ActionButton/ActionButton.component.tsx
+++ src/components/ui/ActionButton/ActionButton.component.tsx
@@ -1,32 +1,65 @@
+import { useEffect, useRef } from 'react'
 import type { ActionButtonProps } from './ActionButton.types'
+import gsap from 'gsap'
+import { useAppStore } from '@/store/useAppStore'
+
+const iconColor = (icon: string | undefined) => {
+	switch (icon) {
+		case 'i-material-symbols-add-circle-outline':
+			return 'bg-blue-500!'
+		case 'i-material-symbols-delete-outline':
+			return 'bg-red-500!'
+		case 'i-material-symbols-light-health-metrics-rounded':
+			return 'bg-emerald-500!'
+		case 'i-icon-park-outline-milk':
+			return 'bg-gray-500!'
+		case 'i-tabler-circles-relation':
+			return 'bg-yellow-500!'
+		case 'i-material-symbols-cancel-outline':
+			return 'bg-red-500!'
+		default:
+			return ''
+	}
+}
 
 export const ActionButton: FC<ActionButtonProps> = ({ icon, ...rest }) => {
-	const iconColor = () => {
-		switch (icon) {
-			case 'i-material-symbols-add-circle-outline':
-				return 'bg-blue-500!'
-			case 'i-material-symbols-delete-outline':
-				return 'bg-red-500!'
-			case 'i-material-symbols-light-health-metrics-rounded':
-				return 'bg-emerald-500!'
-			case 'i-icon-park-outline-milk':
-				return 'bg-gray-500!'
-			case 'i-tabler-circles-relation':
-				return 'bg-yellow-500!'
-			case 'i-material-symbols-cancel-outline':
-				return 'bg-red-500!'
-			default:
-				return ''
+	const btnRef = useRef<HTMLButtonElement>(null)
+	const { loading } = useAppStore()
+
+	// biome-ignore lint:: UseEffect is only called once
+	useEffect(() => {
+		if (btnRef.current && !loading) {
+			gsap.fromTo(
+				btnRef.current,
+				{ scale: 0.6, opacity: 0 },
+				{
+					scale: 1,
+					opacity: 1,
+					duration: 0.5,
+					ease: 'back.out(1.7)',
+				}
+			)
 		}
+	}, [loading])
+
+	const handleMouseEnter = () => {
+		gsap.to(btnRef.current, { scale: 1.1, duration: 0.2, ease: 'power1.out' })
+	}
+
+	const handleMouseLeave = () => {
+		gsap.to(btnRef.current, { scale: 1, duration: 0.2, ease: 'power1.out' })
 	}
 
 	return (
 		<button
+			ref={btnRef}
 			type="button"
 			className="btn btn-circle bg-transparent border-none shadow-none"
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
 			{...rest}
 		>
-			<i className={`${icon} h-7! w-7! ${rest.disabled ? 'bg-gray-400!' : iconColor()}`} />
+			<i className={`${icon} h-7! w-7! ${rest.disabled ? 'bg-gray-400!' : iconColor(icon)}`} />
 		</button>
 	)
 }
