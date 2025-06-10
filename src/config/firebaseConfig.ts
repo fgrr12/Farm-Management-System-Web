b514d130fbef532804618e7e624dd3707e0b46b7
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2025-06-10 14:13:22 -0600

    feat: improve billing card page

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
+
 import {
 	VITE_API_KEY,
 	VITE_APP_ID,
