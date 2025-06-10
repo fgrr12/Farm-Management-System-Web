7d5bdea377af60ea6d6f00f189d2d7a43834b409
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2025-06-10 16:10:59 -0600

    refactor: change services code

--- src/services/employees/index.ts
+++ src/services/employees/index.ts
@@ -6,54 +6,60 @@ import { firestore, signUpAuth } from '@/config/firebaseConfig'
 
 const collectionName = 'users'
 
-export module EmployeesService {
-	export const getEmployees = async (search: string | null, farmUuid: string): Promise<User[]> => {
-		let response = []
+const getEmployees = async (search: string | null, farmUuid: string): Promise<User[]> => {
+	let response = []
 
-		const employeesDocs = await getDocs(
-			query(
-				collection(firestore, collectionName),
-				where('role', 'in', ['employee', 'owner']),
-				where('farmUuid', '==', farmUuid),
-				where('status', '==', true)
-			)
+	const employeesDocs = await getDocs(
+		query(
+			collection(firestore, collectionName),
+			where('role', 'in', ['employee', 'owner']),
+			where('farmUuid', '==', farmUuid),
+			where('status', '==', true)
 		)
-		response = employeesDocs.docs.map((doc) => doc.data()) as User[]
+	)
+	response = employeesDocs.docs.map((doc) => doc.data()) as User[]
 
-		if (search) {
-			response = response.filter(
-				(employee) =>
-					employee.name.toLowerCase().includes(search.toLowerCase()) ||
-					employee.lastName.toLowerCase().includes(search.toLowerCase()) ||
-					employee.email.toLowerCase().includes(search.toLowerCase())
-			) as User[]
-		}
-		return response
+	if (search) {
+		response = response.filter(
+			(employee) =>
+				employee.name.toLowerCase().includes(search.toLowerCase()) ||
+				employee.lastName.toLowerCase().includes(search.toLowerCase()) ||
+				employee.email.toLowerCase().includes(search.toLowerCase())
+		) as User[]
 	}
+	return response
+}
 
-	export const getEmployee = async (uuid: string): Promise<User> => {
-		const document = doc(firestore, collectionName, uuid)
-		const employee = await getDoc(document)
-		return employee.data() as User
-	}
+const getEmployee = async (uuid: string): Promise<User> => {
+	const document = doc(firestore, collectionName, uuid)
+	const employee = await getDoc(document)
+	return employee.data() as User
+}
 
-	export const setEmployee = async (data: User): Promise<void> => {
-		const temporal_psw = 'Pass123!'
-		const response = await createUserWithEmailAndPassword(signUpAuth, data.email, temporal_psw)
-		const { user } = response
+const setEmployee = async (data: User): Promise<void> => {
+	const temporal_psw = 'Pass123!'
+	const response = await createUserWithEmailAndPassword(signUpAuth, data.email, temporal_psw)
+	const { user } = response
 
-		const createdAt = dayjs().toISOString()
-		const document = doc(firestore, collectionName, user.uid)
-		await setDoc(document, { ...data, createdAt, uuid: user.uid })
-	}
+	const createdAt = dayjs().toISOString()
+	const document = doc(firestore, collectionName, user.uid)
+	await setDoc(document, { ...data, createdAt, uuid: user.uid })
+}
 
-	export const updateEmployee = async (data: User): Promise<void> => {
-		const document = doc(firestore, collectionName, data.uuid)
-		await setDoc(document, data, { merge: true })
-	}
+const updateEmployee = async (data: User): Promise<void> => {
+	const document = doc(firestore, collectionName, data.uuid)
+	await setDoc(document, data, { merge: true })
+}
 
-	export const deleteEmployee = async (uuid: string): Promise<void> => {
-		const document = doc(firestore, collectionName, uuid)
-		await setDoc(document, { status: false }, { merge: true })
-	}
+const deleteEmployee = async (uuid: string): Promise<void> => {
+	const document = doc(firestore, collectionName, uuid)
+	await setDoc(document, { status: false }, { merge: true })
+}
+
+export const EmployeesService = {
+	getEmployees,
+	getEmployee,
+	setEmployee,
+	updateEmployee,
+	deleteEmployee,
 }
