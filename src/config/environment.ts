66d99bdc2bf66bc8fc467eb87fbf7935259e8598
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2025-07-23 13:00:03 -0600

    Update environment.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -10,6 +10,6 @@ export const {
 } = import.meta.env
 
 export const isDevelopment =
-	import.meta.env.MODE === 'development' || import.meta.env.MODE === 'local-develop'
+	import.meta.env.MODE === 'develop' || import.meta.env.MODE === 'local-develop'
 export const isProduction = import.meta.env.MODE === 'production'
 export const currentEnvironment = import.meta.env.MODE
