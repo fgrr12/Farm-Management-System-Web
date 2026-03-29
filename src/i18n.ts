2099a74ae2b548dd1990ab93524169af1e1d354e
Fabricio Rojas <32877932+fgrr12@users.noreply.github.com>
2026-03-28 19:58:20 -0600

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,12 +3,44 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
+const ALL_NAMESPACES = [
+	'animal',
+	'animalForm',
+	'animalHealthRecords',
+	'animalProductionRecords',
+	'animalRelations',
+	'animals',
+	'billingCard',
+	'calendar',
+	'common',
+	'dashboard',
+	'dropzone',
+	'employeeForm',
+	'employees',
+	'employeesData',
+	'externalRelationForm',
+	'healthRecordForm',
+	'loginForm',
+	'myAccount',
+	'mySpecies',
+	'notifications',
+	'productionRecordForm',
+	'relatedAnimals',
+	'taskForm',
+	'tasks',
+	'taxDetails',
+	'voice',
+	'voiceRecorder',
+]
+
 i18n
 	.use(Backend)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
 		lng: 'spa',
+		ns: ALL_NAMESPACES,
+		defaultNS: 'common',
 		// debug: true,
 		react: {
 			useSuspense: false,
